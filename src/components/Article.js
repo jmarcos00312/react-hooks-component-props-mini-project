eb3566255836538504a194df15ad8e2e89f948ec
Jeremiah <jeremiahmarcos27@outlook.com>
2021-11-01 11:09:28 -0500

    added the minutes, but couldnt add the emoji and the condition

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
-export default function Article({ title, date = "January 1, 1970", preview }) {
+export default function Article({ title, date = "January 1, 1970", preview, minutes }) {
+    // if (minutes > 30) {
+    //     let 
+        
+    // }
+
     return (
         <article>
             <h3>{title}</h3>
-            <small>{date}</small>
+            <small>{date} • {minutes} to read</small>
             <p>{preview}</p>
+
         </article>
     )
 }
